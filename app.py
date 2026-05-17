50fc4c40ec43d0be64f6669c0cb115be09578c12
A$APk <const.qqdae@gmail.com>
2026-05-18 03:36:48 +0600

    init project

---
+++ app.py
@@ -0,0 +1,41 @@
+import json
+
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+TASKS_FILE = "tasks.json"
+
+def load_tasks():
+    try:
+        with open(TASKS_FILE, "r", encoding="utf-8") as file:
+            return json.load(file)
+
+    except FileNotFoundError:
+        return []
+
+def save_tasks(tasks):
+    with open(TASKS_FILE, "w", encoding="utf-8") as file:
+        json.dump(tasks, file, ensure_ascii=False, indent=4)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    tasks = load_tasks()
+
+    if request.method == "POST":
+
+        task = request.form.get("task")
+
+        if task:
+            tasks.append(task)
+
+            save_tasks(tasks)
+
+        return redirect("/")
+
+    return render_template("index.html", tasks=tasks)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
